9f50c59c47c7a3502fe1c052fdb66fea4ea42b85
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2021-10-24 15:44:19 +0200

    Another solver that doesn't use any external libraries

--- project/src/main.rs
+++ project/src/main.rs
@@ -6,6 +6,7 @@ mod solver;
 use crate::decoder::decoder::decode;
 use crate::preprocesser::preprocesser::preprocess;
 use crate::solver::solver::solve;
+use crate::solver::solver::solve_2;
 
 #[derive(Debug)]
 pub struct ProblemInstance {
@@ -17,7 +18,7 @@ pub struct ProblemInstance {
 fn main() {
     let instance = decode();
     let preprocessed = preprocess(&instance);
-    let result = solve(&preprocessed);
+    let result = solve_2(&preprocessed);
     if result {
         println!("YES")
     }
--- project/src/solver.rs
+++ project/src/solver.rs
@@ -1,4 +1,7 @@
 pub mod solver {
+
+    //TODO: In the case of a YES, your algorithm has to construct
+    // a solution r1...rm and must output the solution
     
     use crate::ProblemInstance;
 
@@ -35,5 +38,77 @@ pub mod solver {
             }
         }
         false
+    }
+
+    pub fn solve_2(p: &ProblemInstance) -> bool {
+        let mut found: bool = false;
+        //let t = &p.t;
+        //println!("{:?}", t);
+        let mut keys: Vec<char> = Vec::new();
+        let mut values: Vec<Vec<String>> = Vec::new();
+        let mut dim_sizes: Vec<usize> = Vec::new();
+        for (key, value) in &(p.r) {
+            keys.push(*key);
+            let val = &*value.clone();
+            values.push(val.to_vec());
+            dim_sizes.push(value.len());
+        }
+        let dims = keys.len();
+        //println!("dims: {}", dims);
+        //println!("keys: {:?}", keys);
+        //println!("values: {:?}", values);
+        //println!("dim_sizes: {:?}", dim_sizes);
+        let partial_result: Vec<(char,String)> = Vec::new();
+        found = recurse_combinations(0, dims, dim_sizes, keys, values, partial_result, (&*p.t).to_vec(), (&*p.s).to_string());
+        return found;
+    }
+
+    pub fn recurse_combinations(current_dim: usize, dims: usize, dim_sizes: Vec<usize>, keys: Vec<char>, values: Vec<Vec<String>>, mut partial_result: Vec<(char,String)>, t: Vec<String>, s: String) -> bool {
+        let mut ret: bool = false;
+        if current_dim >= dims {
+            //println!("{:?}", partial_result);
+            for ti in t {
+                let mut substring = String::new();
+                for c in ti.chars() {
+                    if c.is_lowercase() {
+                        substring.push(c);
+                    } else {
+                        for pr in &partial_result {
+                            if pr.0 == c {
+                                for c2 in pr.1.chars() {
+                                    substring.push(c2);
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+                if !s.contains(&substring) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        for i in 0..dim_sizes[current_dim] {
+            let mut add: bool = true;
+            for j in 0..partial_result.len() {
+                if partial_result[j].0 == keys[current_dim] {
+                    let val: String = values[current_dim][i].clone();
+                    partial_result[j].1 = val;
+                    add = false;
+                    break;
+                }
+            }
+            if add {
+                let val: String = values[current_dim][i].clone();
+                partial_result.push((keys[current_dim], val));
+            }
+
+            ret = recurse_combinations(current_dim + 1, dims, (&*dim_sizes).to_vec(), (&*keys).to_vec(), (&*values).to_vec(), (&*partial_result).to_vec(), (&*t).to_vec(), (&*s).to_string());
+            if ret {
+                break;
+            }
+        }
+        return ret;
     }
 }
